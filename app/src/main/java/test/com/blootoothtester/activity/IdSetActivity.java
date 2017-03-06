cba382d79bbf143cbdcd6a8dfcbcd5dba665a140
Adithya Philip <adithyaphilip@gmail.com>
2017-03-06 21:25:07 +0530

    added permission requests on the first screen

--- app/src/main/java/test/com/blootoothtester/activity/IdSetActivity.java
+++ app/src/main/java/test/com/blootoothtester/activity/IdSetActivity.java
@@ -2,11 +2,16 @@ package test.com.blootoothtester.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.EditText;
 
 import test.com.blootoothtester.R;
+
+import android.content.pm.PackageManager;
 
 public class IdSetActivity extends Activity {
 
@@ -17,6 +22,35 @@ public class IdSetActivity extends Activity {
         setContentView(R.layout.activity_id_set);
 
         mEtOwnAddr = (EditText) findViewById(R.id.et_own_addr);
+
+        makePermissionsRequest();
+    }
+
+    public void makePermissionsRequest() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            String[] permissions = {"android.permission.ACCESS_FINE_LOCATION"};
+            requestPermissions(
+                    permissions, 1
+            );
+            // this takes care of letting the user add the WRITE_SETTINGS permission
+            if (!Settings.System.canWrite(this)) {
+                Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + this.getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        for(int grantResult: grantResults) {
+            if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                makePermissionsRequest();
+                break;
+            }
+        }
     }
 
     public void onNextClick(View v) {
